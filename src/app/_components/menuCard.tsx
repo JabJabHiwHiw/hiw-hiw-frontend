656351de0a73486257abd401dcae041afb3e69f1
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2024-10-19 22:02:04 +0700

    discover page (#4)
    * feat: add components and color
    
    * feat: add image domain
    
    * feat: add type
    
    * feat: add discover page components
    
    * feat: add discover page and layout
    
    * feat: add bun.lock
    
    * fix: fix lint
    
    * fix: add button type, remove close tag

---
+++ src/app/_components/menuCard.tsx
@@ -0,0 +1,60 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+import { faHeart, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
+
+import type { MenuDetail } from '../types'
+import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+export default function MenuCard(props: MenuDetail) {
+  const { id, name, description, imageUrl, isFavorite, isOwner } = props
+  return (
+    <Link href={`/menus/${id}`}>
+      <Card className="flex flex-row w-auto h-[200px] p justify-between">
+        <CardHeader className="h4">
+          <CardTitle className="h4 bold text-black">{name}</CardTitle>
+          <CardDescription className="text-gray-400 h-full">
+            {description}
+          </CardDescription>
+          {isOwner ? (
+            <button type="button" className="w-fit">
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                size={'1x'}
+                className="text-yellow-300"
+              ></FontAwesomeIcon>
+            </button>
+          ) : (
+            <button type="button" className="w-fit">
+              <FontAwesomeIcon
+                icon={isFavorite ? faHeart : faHeartRegular}
+                size={'1x'}
+                className={cn(
+                  'hover:text-yellow-400',
+                  isFavorite ? 'text-yellow-300' : 'text-yellow-500'
+                )}
+              />
+            </button>
+          )}
+        </CardHeader>
+
+        <CardContent className="flex-none relative h-full p-0 w-[200px] ">
+          <Image
+            alt="MenuPic"
+            src={imageUrl}
+            fill={true}
+            className="w-full h-full object-cover rounded-r-xl"
+          />
+        </CardContent>
+      </Card>
+    </Link>
+  )
+}
