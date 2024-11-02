06257363e363e7fa4682e523cf2642c78a770795
Sirivimol Saiyued <faipanda01@gmail.com>
2024-11-02 15:35:03 +0700

    feat: notification

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -7,6 +7,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
+import NotificationItem from './notificationItem'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover'
 
 export default function Navbar() {
   const pathName = usePathname()
@@ -17,6 +23,18 @@ export default function Navbar() {
   //query user data
   const profileImageURL = 'https://www.gstatic.com/webp/gallery/1.jpg'
   const userName = 'John Doe'
+  const notification = [
+    {
+      id: '1',
+      message: 'You have a new notification',
+      expireDate: '2022-01-01',
+    },
+    {
+      id: '2',
+      message: 'You have a new notification',
+      expireDate: '2022-01-01',
+    },
+  ]
 
   return (
     <div className="h4 h-20 w-full bg-primary-300 justify-between items-center flex px-8 py-3">
@@ -37,14 +55,26 @@ export default function Navbar() {
         </Link>
       </div>
       <div className="w-fit flex gap-6 items-center">
-        {/* change notification */}
-        <Link className="text-gray-400" href={'/notification'}>
-          <FontAwesomeIcon
-            icon={faBell}
-            size={'1x'}
-            className="hover:text-neutral-600"
-          />
-        </Link>
+        <Popover>
+          <PopoverTrigger type="button" className="text-gray-400">
+            <FontAwesomeIcon
+              icon={faBell}
+              size={'1x'}
+              className="hover:text-neutral-600"
+            />
+          </PopoverTrigger>
+          <PopoverContent
+            align={'start'}
+            alignOffset={28}
+            side={'left'}
+            sideOffset={-32}
+            className={cn('w-fit lg:w-[440px]')}
+          >
+            {notification.map((item) => (
+              <NotificationItem key={item.id} {...item} />
+            ))}
+          </PopoverContent>
+        </Popover>
         {isProfile ? (
           <Link href={'/create-menu'}>
             <Button variant={'outline'}>Create Menu</Button>
