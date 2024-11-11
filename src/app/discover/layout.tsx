6edf215c28708c575412e64ceaba65bec32c2be3
Sirivimol Saiyued <faipanda01@gmail.com>
2024-11-11 21:46:53 +0700

    fix: remove unused code

--- src/app/discover/layout.tsx
+++ src/app/discover/layout.tsx
@@ -1,16 +1,9 @@
-import { redirect } from 'next/navigation'
 import Navbar from '../_components/navbar'
 export default async function DiscoverLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  //fetch user data
-  const user = 1
-  if (!user) {
-    return redirect('/sign-in')
-  }
-
   return (
     <div className="h-screen w-screen">
       <Navbar />
