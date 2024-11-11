60cc7f156ada8debf2b64a412057658eb0f61dce
Nathathaii <nathathaiaungudomsin@gmail.com>
2024-11-11 22:42:49 +0700

    not finish connecting api

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,6 +3,7 @@ import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 import { UTApi } from "uploadthing/server";
 
+
  
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
--- src/app/menu/[mid]/cook/page.tsx
+++ src/app/menu/[mid]/cook/page.tsx
@@ -4,15 +4,49 @@ import UpdateFridgeTable from "../../_components/UpdateFridgeTable"
 import Link from "next/link"
 import { useRouter } from 'next/navigation';
 import SuccessPopOver from "../../_components/SuccessPopOver";
+import { useEffect , useState} from "react";
+import getMenu from "@/lib/getMenu";
+
 export default function MenuCheck({params}: {params:{mid:string}}){
     const mid = params.mid;
+    const [menuResponse,setMenuResponse] = useState<Menu | null>(null)
+    useEffect(() => {
+      const fetchData = async() =>{
+        const menu = await getMenu(mid)
+        console.log(menu)
+        setMenuResponse(menu)
+      }
+      fetchData()
+    },[mid])
+    // useEffect(() => {
+    //     if (!session) return;
+    //     session.getToken().then((token) => {
+    //         axios
+    //             .get("http://137.184.249.83/food/fridge/expiring", {
+    //                 headers: {
+    //                     Authorization: `Bearer ${token}`,
+    //                 },
+    //             })
+    //             .then((response) => {
+    //                 console.log("response:", response);
+    //                 const notificationData: Notification[] = response.data;
+    //                 console.log("noti:", notificationData);
+    //                 // setNotification(notificationData)
+    //             });
+    //     });
+    // }, [session]);
+    
+
     const router = useRouter();
+    if(!menuResponse) return(
+        <p>Check Menu is loading ...</p>
+    )
     return(
         <div className="flex flex-col items-left p-16 space-y-12">
-            <h1 className="h1 font-bold">Spaghetti With Fresh Tomato Sauce id:{mid}</h1>
+            <h1 className="h1 font-bold">{menuResponse.item.name}</h1>
             <h3 className='h3 font-bold'>Update Fridge Items</h3>
             
-            <UpdateFridgeTable/>
+            <UpdateFridgeTable ingredients={menuResponse.item.ingredients}/>
 
             <div className='flex flex-col space-y-4 pt-8'>
                 <SuccessPopOver name="Update Fridge Items"/>
