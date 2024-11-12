02f43a34b3251ef2aba798a4318eedc95b613071
Nathathaii <nathathaiaungudomsin@gmail.com>
2024-11-12 15:00:07 +0700

    rebase main

--- src/app/menu/[mid]/cook/page.tsx
+++ src/app/menu/[mid]/cook/page.tsx
@@ -3,10 +3,21 @@ import { Button } from "@/components/ui/button"
 import UpdateFridgeTable from "../../_components/UpdateFridgeTable"
 import Link from "next/link"
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import getMenu from "@/lib/getMenu";
 
-export default function MenuCheck({params,menuName}: {params:{mid:string}, menuName:string}){
-    const mid = params.mid;
+export default function MenuCheck({params}:{params:{mid:string}}){
     const router = useRouter();
+    const [menuName,setMenuName] = useState<string|null>(null);
+    const mid = params.mid;
+    useEffect(() => {
+        const fetchData = async() =>{
+          const menu = await getMenu(mid)
+          console.log(menu)
+          setMenuName(menu.item.name)
+        }
+        fetchData()
+      },[mid])
     return(
         <div className="flex flex-col items-left p-16 space-y-12">
             <h1 className="h1 font-bold">{menuName}</h1>
