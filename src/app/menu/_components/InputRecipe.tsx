16046b8aa85653d87acce4fbc5bda1de69034aeb
Supawit Saeliew <63398464+JayJacka@users.noreply.github.com>
2024-11-12 20:04:24 +0700

    fix: build failed

--- src/app/menu/_components/InputRecipe.tsx
+++ src/app/menu/_components/InputRecipe.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
