e9ffe7d7232c74a5e20bd6c8af81778593e32be7
Nathathaii <nathathaiaungudomsin@gmail.com>
2024-11-12 14:14:23 +0700

    envi

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,5 +1,4 @@
 export default async function getMenu(id:string){
-    //const menu_url = process.env.MENU_SERVICE_ENDPOINT
     const menu_url = 'http://137.184.249.83/food/menu'
     console.log("menu_url = " + menu_url)
     const response = await fetch(`${menu_url}/${id}`)
